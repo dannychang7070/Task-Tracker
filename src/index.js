8ae74a865ec0aaa0a9d18812e11748774018f0cc
Danny Chang <dannychang@Dannys-MacBook-16.local>
2023-06-24 16:54:40 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'; // npm run server
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
